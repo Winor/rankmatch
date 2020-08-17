1d71d8a8b6f895b14eaae46a45e41e7139d466a8
Or Benjamin <or.b@me.com>
2020-08-18 00:32:32 +0300

    first commit

---
+++ players.js
@@ -0,0 +1,218 @@
+'use strict'
+module.exports = [{
+    player_name: 'winor',
+    wantstoplay: 'tank',
+    tank_rank: 2222,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'buythebug',
+    wantstoplay: 'support',
+    tank_rank: 2222,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'gosu',
+    wantstoplay: 'dps',
+    tank_rank: 2222,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'sbdd',
+    wantstoplay: 'tank',
+    tank_rank: 3445,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'altofox',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'nootnoot',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 3435,
+    dps_rank: 2435,
+    hasteam: false
+},
+{
+    player_name: 'gfhd',
+    wantstoplay: 'tank',
+    tank_rank: 1445,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'raymon',
+    wantstoplay: 'support',
+    tank_rank: 2222,
+    support_rank: 2456,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'sheepr',
+    wantstoplay: 'dps',
+    tank_rank: 2222,
+    support_rank: 3435,
+    dps_rank: 1435,
+    hasteam: false
+},
+{
+    player_name: 'loopingolf',
+    wantstoplay: 'tank',
+    tank_rank: 1445,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'gfjhdb',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 1535,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'verus',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 2135,
+    hasteam: false
+},
+{
+    player_name: 'arwith',
+    wantstoplay: 'tank',
+    tank_rank: 3145,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'philp',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 2135,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'normalaf',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 1135,
+    hasteam: false
+} ,
+{
+    player_name: 'zink',
+    wantstoplay: 'tank',
+    tank_rank: 1115,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'broken',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 3735,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'nevermind',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 1935,
+    hasteam: false
+},
+{
+    player_name: 'varrick',
+    wantstoplay: 'tank',
+    tank_rank: 2545,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'zhuli',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 2705,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'nimo',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 2165,
+    hasteam: false
+},
+{
+    player_name: 'beep',
+    wantstoplay: 'tank',
+    tank_rank: 2995,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'zopp',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 1005,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'galaco',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 1115,
+    hasteam: false
+} ,
+{
+    player_name: 'nokitoki',
+    wantstoplay: 'tank',
+    tank_rank: 2945,
+    support_rank: 3435,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'godlike',
+    wantstoplay: 'support',
+    tank_rank: 1446,
+    support_rank: 3705,
+    dps_rank: 3435,
+    hasteam: false
+},
+{
+    player_name: 'xlopop',
+    wantstoplay: 'dps',
+    tank_rank: 1446,
+    support_rank: 2435,
+    dps_rank: 2151,
+    hasteam: false
+}
+]

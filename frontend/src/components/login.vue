f857ed73e04570fe8c8bb066a3010e5da4c24b45
Or Benjamin <or.b@me.com>
2020-09-12 15:10:50 +0300

    added firebase and vue router

---
+++ frontend/src/components/login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="login">
+      <section id="firebaseui-auth-container"></section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import {authUI}  from "../firebase";
+import "firebaseui/dist/firebaseui.css";
+
+@Component
+export default class PlayerName extends Vue {
+  @Prop() private msg!: string;
+  mounted() {
+        authUI();
+    }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.login {
+  padding-top: 10%;
+}
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,39 @@ import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import { auth } from "./firebase";
 
 Vue.config.productionTip = false
 Vue.use(Element)
 Vue.use(Vuex)
+Vue.use(VueRouter)
+
+import RankForm from "./components/RankForm.vue";
+import login from "./components/login.vue";
+
+const routes = [
+  { path: '/', name: 'login', component: login },
+  { path: '/form', name: 'form', component: RankForm, meta: { requiresAuth: true } }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  auth.onAuthStateChanged(() => {
+    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+    if (auth.currentUser && to.name === 'login') {
+      next('/form')
+    } else if (requiresAuth && !auth.currentUser) {
+      next('/')
+    } else {
+      next()
+    }
+  })
+})
 
 const store = new Vuex.Store({
   state: {},
@@ -15,6 +44,7 @@ const store = new Vuex.Store({
 })
 
 new Vue({
+  router,
   store,
   render: h => h(App),
 }).$mount('#app')

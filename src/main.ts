78e3bac3ed4c3a2e554875b254d97a0013d8f65a
Or Benjamin <or.b@me.com>
2020-09-19 19:10:01 +0300

    set document.title by route name

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ import RankForm from "./components/RankForm.vue";
 import login from "./components/login.vue";
 
 const routes = [
-  { path: '/', name: 'login', component: login },
-  { path: '/form', name: 'form', component: RankForm, meta: { requiresAuth: true } }
+  { path: '/', name: 'Login', component: login },
+  { path: '/form', name: 'Form', component: RankForm, meta: { requiresAuth: true } }
 ]
 
 const router = new VueRouter({
@@ -26,8 +26,11 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   auth.onAuthStateChanged(() => {
+    Vue.nextTick(() => {
+      document.title = 'Rank Updater - ' + to.name || 'Rank Updater';
+  });
     const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
-    if (auth.currentUser && to.name === 'login') {
+    if (auth.currentUser && to.name === 'Login') {
       next('/form')
     } else if (requiresAuth && !auth.currentUser) {
       next('/')
